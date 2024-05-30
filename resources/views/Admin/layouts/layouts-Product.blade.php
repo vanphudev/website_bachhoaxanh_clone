b399285c94d438b1817931cc586aa106201ad6d5
Hoang13052003 <132593416+Hoang13052003@users.noreply.github.com>
2024-05-31 03:14:47 +0700

    update manager product index

--- resources/views/Admin/layouts/layouts-Product.blade.php
+++ resources/views/Admin/layouts/layouts-Product.blade.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/stylecss-products-admin.css') }}" />
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
     <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
