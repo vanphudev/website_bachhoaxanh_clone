e61ba09b89b9bb2859df8f27ef02628ecbec2ad4
vanphudev <nguyenvanphu.hufi@gmail.com>
2024-06-02 18:29:53 +0700

    update lan 13

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -6,38 +6,171 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
-
+use PDO;
 
 class CardsController extends Controller
 {
+
     public function index()
     {
         // Kiểm tra xem người dùng đã đăng nhập chưa.
         if (!Cookie::get('user_data')) {
             return redirect()->route('Login')->with('error', 'Vui lòng đăng nhập để xem giỏ hàng !');
         }
-
         // Lấy thông tin người dùng từ cookie.
         $user_data = json_decode(Crypt::decryptString(Cookie::get('user_data')), true);
         if (!$user_data) {
             return redirect()->route('Login')->with('error', 'Vui lòng đăng nhập để xem giỏ hàng !');
         }
-
         // Lấy thông tin người dùng từ database.
         $user = DB::table('khachhang')->where('MAKH', $user_data['id'])->first();
-
         // Lấy thông tin giỏ hàng từ database dựa vào id người dùng.
         $cart = DB::table('cards')->where('MAKH', $user->MAKH)->first();
 
+
         if (isset($cart)) {
-            // Nếu có giỏ hàng thì trả về view với thông tin người dùng và giỏ hàng.
-            // Truy vấn lấy thông tin giỏ hàng.
-            $detailed_cart = DB::table('detail_cards')->where('ID_CARD', $cart->ID_CARD)->get();
-            return view('cards.index', compact('user', 'cart', 'detailed_cart'));
-        } else {
-            return view('cards.index', compact('user', 'cart'));
+            $detail_cart = DB::table('detail_cards')
+                ->where('ID_CARD', $cart->ID_CARD)
+                ->join('MAT_HANG', 'MAT_HANG.MAMH', '=', 'detail_cards.MAMH')
+                ->leftJoin('GIAM_GIA', function ($join) {
+                    $join->on('GIAM_GIA.MAMH', '=', 'MAT_HANG.MAMH')
+                        ->where('GIAM_GIA.LAN_GIAM_GIA', DB::raw('(SELECT MAX(LAN_GIAM_GIA) FROM GIAM_GIA WHERE GIAM_GIA.MAMH = MAT_HANG.MAMH)'));
+                })
+                ->select(
+                    'MAT_HANG.MAMH',
+                    'MAT_HANG.TENMH',
+                    'MAT_HANG.PICTURE',
+                    'MAT_HANG.DONVITINH',
+                    'MAT_HANG.GIA_BAN',
+                    'MAT_HANG.SO_GAM',
+                    'MAT_HANG.KHOILUONG',
+                    'detail_cards.SOLUONG',
+                    'GIAM_GIA.TILE_GIAM_GIA',
+                    DB::raw('CASE
+                    WHEN MAT_HANG.KHOILUONG = 0 THEN
+                        CASE
+                            WHEN GIAM_GIA.TILE_GIAM_GIA IS NOT NULL THEN (1 - (GIAM_GIA.TILE_GIAM_GIA / 100)) * MAT_HANG.GIA_BAN * detail_cards.SOLUONG
+                            ELSE MAT_HANG.GIA_BAN * detail_cards.SOLUONG
+                        END
+                    WHEN MAT_HANG.KHOILUONG = 1 THEN
+                        CASE
+                            WHEN GIAM_GIA.TILE_GIAM_GIA IS NOT NULL THEN (((MAT_HANG.SO_GAM * MAT_HANG.GIA_BAN)/1000) * detail_cards.SOLUONG) * (1 - (GIAM_GIA.TILE_GIAM_GIA / 100))
+                            ELSE ((MAT_HANG.SO_GAM * MAT_HANG.GIA_BAN)/1000) * detail_cards.SOLUONG
+                        END
+                    ELSE
+                        NULL
+                    END AS THANH_TIEN'),
+                    DB::raw('CASE
+                    WHEN MAT_HANG.KHOILUONG = 0 THEN
+                        CASE
+                            WHEN GIAM_GIA.TILE_GIAM_GIA IS NOT NULL THEN (1 - (GIAM_GIA.TILE_GIAM_GIA / 100)) * MAT_HANG.GIA_BAN
+                            ELSE MAT_HANG.GIA_BAN
+                        END
+                    WHEN MAT_HANG.KHOILUONG = 1 THEN
+                        CASE
+                            WHEN GIAM_GIA.TILE_GIAM_GIA IS NOT NULL THEN ((MAT_HANG.SO_GAM * MAT_HANG.GIA_BAN)/1000) * (1 - (GIAM_GIA.TILE_GIAM_GIA / 100))
+                            ELSE ((MAT_HANG.SO_GAM * MAT_HANG.GIA_BAN)/1000)
+                        END
+                    ELSE
+                        NULL
+                    END AS GIA_BAN_NEW')
+                )
+                ->get();
+            $tongtien = 0;
+            foreach ($detail_cart as $item) {
+                $tongtien += $item->THANH_TIEN;
+            }
+            $count = count($detail_cart);
+            return view('cards.index', compact('user', 'cart', 'detail_cart', 'tongtien', 'count'));
         }
+        return view('cards.index', compact('user', 'cart'));
         return view('cards.index');
+    }
+
+    public function RemoveProductFromCart(Request $request)
+    {
+        // Kiểm tra xem người dùng đã đăng nhập chưa.
+        if (!Cookie::get('user_data')) {
+            return response()->json([
+                'success' => false,
+                'redirect_url' => route('Login'),
+                'message' => 'Vui lòng đăng nhập để xóa sản phẩm khỏi giỏ hàng !'
+            ]);
+        }
+        $user_data = json_decode(Crypt::decryptString(Cookie::get('user_data')), true);
+        if (!$user_data) {
+            return response()->json([
+                'success' => false,
+                'redirect_url' => route('Login'),
+                'message' => 'Vui lòng đăng nhập để xóa sản phẩm khỏi giỏ hàng !'
+            ]);
+        }
+
+        $user = DB::table('khachhang')->where('MAKH', $user_data['id'])->first();
+        $product_id = $request->input('product_id');
+        $cart = DB::table('cards')->where('MAKH', $user->MAKH)->first();
+        if (isset($cart)) {
+            $detail_card = DB::table('detail_cards')->where('ID_CARD', $cart->ID_CARD)->where('MAMH', $product_id)->first();
+            if (isset($detail_card)) {
+                DB::table('detail_cards')->where('ID_CARD', $cart->ID_CARD)->where('MAMH', $product_id)->delete();
+                return response()->json([
+                    'success' => true
+                ]);
+            } else {
+                return response()->json([
+                    'success' => false
+                ]);
+            }
+        } else {
+            return response()->json([
+                'success' => false
+            ]);
+        }
+    }
+
+    public function UpdateProductQuantity(Request $request)
+    {
+        if (!Cookie::get('user_data')) {
+            return response()->json([
+                'success' => false,
+                'redirect_url' => route('Login'),
+                'message' => 'Vui lòng đăng nhập để cập nhật số lượng sản phẩm trong giỏ hàng !'
+            ]);
+        }
+        $user_data = json_decode(Crypt::decryptString(Cookie::get('user_data')), true);
+        if (!$user_data) {
+            return response()->json([
+                'success' => false,
+                'redirect_url' => route('Login'),
+                'message' => 'Vui lòng đăng nhập để cập nhật số lượng sản phẩm trong giỏ hàng !'
+            ]);
+        }
+        $user = DB::table('khachhang')->where('MAKH', $user_data['id'])->first();
+        $product_id = $request->input('product_id');
+        $quantity = $request->input('quantity');
+        $cart = DB::table('cards')->where('MAKH', $user->MAKH)->first();
+        if (isset($cart)) {
+            $detail_card = DB::table('detail_cards')->where('ID_CARD', $cart->ID_CARD)->where('MAMH', $product_id)->first();
+            if (isset($detail_card)) {
+                DB::table('detail_cards')->where('ID_CARD', $cart->ID_CARD)->where('MAMH', $product_id)->update([
+                    'SOLUONG' => $quantity
+                ]);
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Số lượng sản phẩm đã được cập nhật.'
+                ]);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Sản phẩm không tồn tại trong giỏ hàng.'
+                ]);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Giỏ hàng không tồn tại.'
+            ]);
+        }
     }
 
     public function AddToCart(Request $request)
